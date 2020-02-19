fe37579dd8cdb76a46cff7c51b0e272f1db2233e
caiyy <caiyy@caiyydeMac-Pro.local>
2020-02-20 02:59:11 +0800

    修改插件排序

--- files/root/usr/lib/lua/luci/controller/systemup.lua
+++ files/root/usr/lib/lua/luci/controller/systemup.lua
@@ -1,7 +1,7 @@
 module("luci.controller.systemup", package.seeall)
 
 function index()
-        entry({"admin", "services", "systemup"}, alias("admin", "services", "systemup", "client"),_("自动升级系统"), 100).dependent = true
+        entry({"admin", "services", "systemup"}, alias("admin", "services", "systemup", "client"),_("自动升级系统"), 10).dependent = true
         entry({"admin", "services", "systemup", "client"}, cbi("systemup/client"),_("设置"), 10).leaf = true
         entry({"admin", "services", "systemup", "log"}, cbi("systemup/log"),_("日志"), 101).leaf = true
 end
