785c1ccdc2511eee54b6eeed4956cc0226aebaa7
caiyy <yylcy@vip.qq.com>
2020-03-14 12:35:31 +0000

    调整显示顺序

--- files/root/usr/lib/lua/luci/controller/systemup.lua
+++ files/root/usr/lib/lua/luci/controller/systemup.lua
@@ -1,7 +1,7 @@
 module("luci.controller.systemup", package.seeall)
 
 function index()
-        entry({"admin", "services", "systemup"}, alias("admin", "services", "systemup", "setup"),_("自动升级系统"), 10).dependent = true
-        entry({"admin", "services", "systemup", "setup"}, cbi("systemup/setup"),_("设置"), 10).leaf = true
+        entry({"admin", "services", "systemup"}, alias("admin", "services", "systemup", "setup"),_("自动升级系统"), 1).dependent = true
+        entry({"admin", "services", "systemup", "setup"}, cbi("systemup/setup"),_("设置"), 1).leaf = true
         entry({"admin", "services", "systemup", "log"}, cbi("systemup/log"),_("日志"), 101).leaf = true
 end
