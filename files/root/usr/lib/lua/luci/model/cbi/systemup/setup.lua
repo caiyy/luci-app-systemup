31045d3552374e111f57ef8a20e99325fec4d793
caiyy <yylcy@vip.qq.com>
2020-03-15 01:44:33 +0000

    测试

--- files/root/usr/lib/lua/luci/model/cbi/systemup/setup.lua
+++ files/root/usr/lib/lua/luci/model/cbi/systemup/setup.lua
@@ -1,11 +1,12 @@
 require("luci.sys")
 
-m = Map("systemup", translate("自动升级系统"))
+m = Map("systemup", translate("自动升级系统 V5.0.7.3"))
 
 s = m:section(TypedSection, "base_arg", "")
 s.addremove = false
 s.anonymous = true
-
+print(m)
+-- make package/openwrt-packages/luci-app-systemup/compile -j5
 s:option(Value, "firmware_url", translate("固件服务器地址"), "格式:http://domain/xxx,不要有/结尾")
 s:option(Value, "firmware_name", translate("固件的文件名字"), "比如 openwrt-x86-64-combined-squashfs.img.gz")
 s:option(Value, "firmware_sha256sum", translate("固件sha256sum"), "固件的sha256sum名字")
